d749543509aba62294211e9943e5546730b4a972
canerigus <61908293+canerigus@users.noreply.github.com>
2025-11-10 09:35:33 +0300

    updates and refactors. example.html

--- src/routes/materialIconRoute.js
+++ src/routes/materialIconRoute.js
@@ -1,19 +1,35 @@
 import express from "express";
-import getMaterialDesignIcon from "../core/materialIcons.js";
+import {
+	getMaterialIcon,
+	getPeepsMonochromaticIcon,
+	getPeepsMultiColorIcon,
+	getAllPeepsIcons,
+	getAllHumaaansIcons,
+	getHumaaansIcon,
+} from "../core/materialIcons.js";
 import sharp from "sharp";
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-router.get("/", (req, res) => {
-	try {
-		const { name, style, size, color } = req.query;
+const defaults = {
+	color: "00C0E7",
+	size: "32",
+	mdiStyle: "filled",
+};
 
-		if (!name) {
-			return res.status(400).send("Name is required");
+router.get("/mdiIcons/:iconStyle/:iconName/:iconSize/:iconColor", (req, res) => {
+	try {
+		const { iconName, iconStyle, iconSize, iconColor } = req.params;
+
+		if (!iconName) {
+			return res.status(400).send("Icon is required");
 		}
 
-		const iconSvg = getMaterialDesignIcon({ name, style, size, color });
+		const iconSvg = getMaterialIcon({ iconName,
+			iconStyle: iconStyle || defaults.mdiStyle,
+			iconSize: iconSize || defaults.size,
+			iconColor: iconColor || defaults.color });
 
 		res.set({
 			"Content-Type": "image/png",
@@ -23,7 +39,89 @@ router.get("/", (req, res) => {
 
 		sharp(Buffer.from(iconSvg)).png().pipe(res);
 	} catch (error) {
-		console.error(`Error on icon GET: ${error.message}`);
+		console.error(`Error on mdi icon GET: ${error.message}`);
+		res.status(500).send("something went wrong");
+	}
+});
+
+router.get("/openPeepsIcons/:iconStyle/:iconName/:iconSize/:iconColor.png", (req, res) => {
+	try {
+		const { iconName, iconStyle, iconSize, iconColor } = req.params;
+
+		if (!iconName) {
+			return res.status(400).send("Icon is required");
+		}
+		let iconSvg;
+		if (iconStyle === "monochromatic") {
+			iconSvg = getPeepsMonochromaticIcon({
+				iconName,
+				iconSize: iconSize || defaults.size,
+				iconColor: iconColor || defaults.color,
+			});
+		} else {
+			iconSvg = getPeepsMultiColorIcon({
+				iconName,
+				iconSize: iconSize || defaults.size,
+			});
+		}
+
+		res.set({
+			"Content-Type": "image/png",
+			"Cache-Control": "no-cache, no-store, must-revalidate",
+			Expires: "0",
+		});
+
+		sharp(Buffer.from(iconSvg)).png().pipe(res);
+	} catch (error) {
+		console.error(`Error on openPeepsSvgs GET: ${error.message}`);
+		res.status(500).send("something went wrong");
+	}
+});
+
+router.get("/openPeepsIcons/svgs", async (req, res) => {
+	try {
+		const openPeepsSvgs = await getAllPeepsIcons();
+		res.json(openPeepsSvgs);
+	} catch (error) {
+		console.error(`Error on all openPeepsIcons GET: ${error.message}`);
+		res.status(500).send("something went wrong");
+	}
+});
+
+router.get("/humaaans/:iconStyle/:iconName/:iconSize/:iconColor.png", (req, res) => {
+	try {
+		const { iconStyle, iconName, iconSize, iconColor } = req.params;
+
+		if (!iconName) {
+			return res.status(400).send("Icon is required");
+		}
+
+		const iconSvg = getHumaaansIcon({
+			iconStyle,
+			iconName,
+			iconSize,
+			iconColor,
+		});
+
+		res.set({
+			"Content-Type": "image/png",
+			"Cache-Control": "no-cache, no-store, must-revalidate",
+			Expires: "0",
+		});
+
+		sharp(Buffer.from(iconSvg)).png().pipe(res);
+	} catch (error) {
+		console.error(`Error on humaaans GET: ${error.message}`);
+		res.status(500).send("something went wrong");
+	}
+});
+
+router.get("/humaaans/svgs", async (req, res) => {
+	try {
+		const humaaansSvgs = await getAllHumaaansIcons();
+		res.json(humaaansSvgs);
+	} catch (error) {
+		console.error(`Error on all humaaans GET: ${error.message}`);
 		res.status(500).send("something went wrong");
 	}
 });
--- src/server.js
+++ src/server.js
@@ -3,6 +3,9 @@ import nconf from "nconf";
 import cors from "cors";
 import { createTerminus } from "@godaddy/terminus";
 import materialIconRoute from "./routes/materialIconRoute.js";
+import fs from "fs";
+import iconNamesArray from "./assets/iconNamesArray.js";
+const iconsHtml = fs.readFileSync("./assets/icons.html", "utf-8");
 
 nconf.argv().file({ file: "config.json" || "./config.json" });
 const app = express();
@@ -27,7 +30,16 @@ if (!configFile.port) {
 	process.exit(1);
 }
 
-app.use("/material-icon", materialIconRoute);
+app.use("/", materialIconRoute);
+
+app.get("/example", (req, res) => {
+	res.setHeader("Content-Type", "text/html");
+	res.status(200).send(iconsHtml);
+});
+
+app.get("/iconNamesArray", (req, res) => {
+	res.status(200).send(iconNamesArray);
+});
 
 const server = app.listen(configFile.port, (err) => {
 	if (err) {
